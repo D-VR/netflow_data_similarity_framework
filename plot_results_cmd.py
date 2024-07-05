ce3c05b26bb99e2d00236ba6b718c96fffb289bb
maxwolf-code <maxwolfpublic@gmail.com>
2024-07-05 15:19:43 +0200

    update plotting

--- plot_results_cmd.py
+++ plot_results_cmd.py
@@ -196,8 +196,267 @@ def split_ds_file(df):
         df = df.set_index('step')
         return df.sort_index()
 
-if __name__ == '__main__':
+        
+def plot_history_mean_metric(df_real, df_synthetic, store_path, df_error):
+    def calc_mean(df_results):
+        mean_columns = ['Jensen-Shannon Div. Mean', 
+                        #'Wasserstein Dist. Mean x10', 
+                        'Pearson MAE',
+                        'Correlation Ratio MAE', 
+                        'Theils U MAE', 
+                        
+                        'IF Discriminator TSTR FPR', #calc 1-metric for value where 0-1, 0 is better
+                        'IF Discriminator TRTS FPR', 
+                        'OCSVM Discriminator TSTR FPR', #calc 1-metric for value where 0-1, 0 is better
+                        'OCSVM Discriminator TRTS FPR', 
+                        #'Task TSTR F1-Score-Macro',
+                        #'Task TRTS F1-Score-Macro',
+                        #'Task TSTR F1-Score-Micro',
+                        #'Task TRTS F1-Score-Micro', 
+                        'IF Task TSTR F1-Score-Weighted',
+                        'IF Task TRTS F1-Score-Weighted',
+                        'OCSVM Task TSTR F1-Score-Weighted',
+                        'OCSVM Task TRTS F1-Score-Weighted',
+                        'XGB Task TSTR F1-Score-Weighted',
+                        'XGB Task TRTS F1-Score-Weighted',
+        ]
+
+        invert_cols = ['IF Discriminator TSTR FPR', #calc 1-metric for value where 0-1, 0 is better
+                        'IF Discriminator TRTS FPR', 
+                        'OCSVM Discriminator TSTR FPR', #calc 1-metric for value where 0-1, 0 is better
+                        'OCSVM Discriminator TRTS FPR', 
+                        #'Task TSTR F1-Score-Macro',
+                        #'Task TRTS F1-Score-Macro',
+                        #'Task TSTR F1-Score-Micro',
+                        #'Task TRTS F1-Score-Micro', 
+                        'IF Task TSTR F1-Score-Weighted',
+                        'IF Task TRTS F1-Score-Weighted',
+                        'OCSVM Task TSTR F1-Score-Weighted',
+                        'OCSVM Task TRTS F1-Score-Weighted',
+                        'XGB Task TSTR F1-Score-Weighted',
+                        'XGB Task TRTS F1-Score-Weighted',
+        ]
+        
+        data_metrics = ['Jensen-Shannon Div. Mean', 
+                        #'Wasserstein Dist. Mean x10', 
+                        'Pearson MAE',
+                        'Correlation Ratio MAE', 
+                        'Theils U MAE', 
+                        
+                        'IF Discriminator TSTR FPR', #calc 1-metric for value where 0-1, 0 is better
+                        'IF Discriminator TRTS FPR', 
+                        'OCSVM Discriminator TSTR FPR', #calc 1-metric for value where 0-1, 0 is better
+                        'OCSVM Discriminator TRTS FPR',  ]
+                    
+        domain_metrics = [ 'IF Task TSTR F1-Score-Weighted',
+                        'IF Task TRTS F1-Score-Weighted',
+                        'OCSVM Task TSTR F1-Score-Weighted',
+                        'OCSVM Task TRTS F1-Score-Weighted',
+                        'XGB Task TSTR F1-Score-Weighted',
+                        'XGB Task TRTS F1-Score-Weighted',
+                        'Error_Flows']
+                    
+        #invert values
+        df_results[invert_cols] = 1- df_results[invert_cols]
+        df_results['Data Dissimilarity Score'] = df_results[data_metrics].mean(axis=1)
+        df_results['Domain Dissimilarity Score'] = df_results[domain_metrics].mean(axis=1)
+
+        df_results['Data Dissimilarity Score_std'] = df_results[data_metrics].std(axis=1)
+        df_results['Domain Dissimilarity Score_std'] = df_results[domain_metrics].std(axis=1)
+        print(df_results)
+        df_results = df_results[['ds_1', 'sample_1', 'ds_2', 'sample_2', 'model', 'Data Dissimilarity Score', 'Domain Dissimilarity Score', 'Data Dissimilarity Score_std', 'Domain Dissimilarity Score_std']]
+        #create mean for data and task(with syntax errors) metrics, seperate plots
+        return df_results
+    #print("START")
+    #print(df_synthetic)
+    #print(df_synthetic[df_synthetic['sample_1']=='step-0500'])
+    #TODO combine similarity with error flows rate..
+
+    df_error['ds_1'] = df_error['ds']
+    df_error=df_error.reset_index()
+    df_error['sample_1'] = 0#df_error['step']
+    df_error['Error_Flows'] = df_error['Flows']/10000
+    #print(df_error)
+    #print(df_error.columns)
+    df_error = df_error[['ds_1', 'sample_1', 'model', 'Error_Flows']]
+    print('Error')
+    print(df_error)
+    print(df_error.columns)
+    #df_error=df_error.drop(index=list(range(500,10000, 1000))) #drop intermediate steps
+    #print(df_error)
+
+    #df_synthetic['sample_1'] = df_synthetic['sample_1'].str.split('-').str[1].astype(int) #remove step- prefix
+    #still necessary
+    print("Synthetic")
+    print(df_synthetic[['ds_1', 'model', 'sample_1']])
+    df_synthetic = df_synthetic.merge(df_error, how='inner', left_on=['ds_1', 'model', 'sample_1'], right_on=['ds_1', 'model', 'sample_1'])
+
+    df_real['Error_Flows'] = 0 #real data have no syntax errors
+    print("Synthetic merge")
+    print(df_synthetic[['ds_1', 'sample_1', 'ds_2', 'sample_2', 'model', 'Error_Flows']])
+    print(df_synthetic.columns)
+    quit()
+
+
+    df_real = calc_mean(df_real)
+    #print(df_synthetic)
+    #print(df_synthetic[df_synthetic['sample_1']==500])
+
+    df_synthetic = calc_mean(df_synthetic)
+    #print("DF_SYNTETIC")
+    #print(df_synthetic)
+    #print(df_synthetic[df_synthetic['sample_1']==500])
+
+    #quit()
+    #df_synthetic['error_flows'] = np.where( ( (df_synthetic['ds_1']==df_error['ds']) & (df_synthetic['model']==df_error['model']) & (df_synthetic['sample_1']==df_error.index)), df_error['Flows'] )
+    #df_synthetic['error_flows'] = np.where( ( (df_synthetic['ds_1']==df_error['ds']) & (df_synthetic['model']==df_error['model']) & (df_synthetic['sample_1']==df_error.index)), df_error['Flows'] )
+
+    #df_synthetic['error_flows'] = np.where( ( (df_synthetic['ds_1']==df_error['ds']) & (df_synthetic['model']==df_error['model']) & (df_synthetic['sample_1']==df_error.index)), df_error['Flows'] )
+
     
+
+    # plotting
+    def benchmark_line_plot_history(df_real, synthetic_data_points, store_path):
+        name_list = df_real.columns.drop(['ds_1', 'sample_1', 'ds_2', 'sample_2', 'model'])
+        name_list = [n for n in name_list if not ('std' in n)] #remove std values, will be used later
+        #print(name_list)
+        #quit()
+
+        for ds_group in synthetic_data_points['ds_1'].unique():
+            df_synthetic_filter = synthetic_data_points[synthetic_data_points['ds_1'].str.contains(ds_group, case=False) & synthetic_data_points['ds_2'].str.contains(ds_group, case=False)]
+            df_real_filter = df_real[df_real['ds_1'].str.contains(ds_group, case=False) & df_real['ds_2'].str.contains(ds_group, case=False)]
+            
+            #df_real_filter_other = df_real[(~ df_real['ds_1'].str.contains(ds_group, case=False)) &  (~ df_real['ds_2'].str.contains(ds_group, case=False))]
+            df_real_filter_other = df_real[df_real['ds_1'].str.contains(ds_group, case=False) ^ df_real['ds_2'].str.contains(ds_group, case=False)]
+            #df_real_filter_other = df_real
+
+            print("TARGET:")
+            print(df_real_filter['ds_1'].unique())
+            print(df_real_filter['ds_2'].unique())
+            print('OTHER')
+            print(df_real_filter_other['ds_1'].unique())
+            print(df_real_filter_other['ds_2'].unique())
+
+            df_real_stats = df_real_filter.describe()
+            df_real_stats_other = df_real_filter_other.describe()
+            #print(df_real_stats)
+            #print(df_real_stats_other)
+
+            #TODO filter important data firest (thresholds + datapoints)
+            #plot afterwareds
+
+            #fig, axs = plt.subplots(len(name_list))
+            fig = plt.figure(figsize = (3,7))
+            #plt.yticks([0, 0.25,0.5,0.75,1.0])
+
+            gs = fig.add_gridspec(len(name_list), hspace=0.4)
+            #axs = gs.subplots(sharex=True, sharey=True)
+            axs = gs.subplots(sharex=True, sharey=False)
+            #print(axs)
+            #quit()
+            #axs = [axs]
+
+
+            for axs_num, col in enumerate(name_list):
+                print(col)
+                val_25 = df_real_stats.loc['25%', col]
+                val_75 = df_real_stats.loc['75%', col]
+                val_mean = df_real_stats.loc['mean', col]
+                val_min = df_real_stats.loc['min', col]
+                val_max = df_real_stats.loc['max', col]
+
+                val_25_other = df_real_stats_other.loc['25%', col]
+                val_75_other = df_real_stats_other.loc['75%', col]
+                val_mean_other = df_real_stats_other.loc['mean', col]
+                val_min_other = df_real_stats_other.loc['min', col]
+                val_max_other = df_real_stats_other.loc['max', col]
+                #print(val_25)
+                #fig = plt.figure(figsize=(8,8))
+                tmp = df_synthetic_filter[['sample_1', col, col+'_std', 'model']]
+                print(tmp)
+                tmp.set_index('sample_1', inplace=True)
+                print(tmp)
+                
+                axs[axs_num].tick_params(left = True, right = False , labelleft = True ,
+                    labelbottom = True, bottom = False)
+                
+                #remove spine lines
+                axs[axs_num].spines['right'].set_visible(False)
+                axs[axs_num].spines['left'].set_visible(True)
+                axs[axs_num].spines['top'].set_visible(False)
+                axs[axs_num].spines['bottom'].set_visible(True)
+                #plot_height = -0.1
+
+                pmin = 0
+                pmax=10000
+                #pmax = plot_height*1.1
+
+                #TODO annotate lines
+
+                dates = list(tmp[col].values) + [val_25_other, val_75_other, val_mean_other, val_25, val_75, val_mean]
+                names = ['WGAN', 'GPT', '25%', '75%', 'Mean', '25%', '75%', 'Mean']
+
+                # annotate lines
+                zip_list = zip(dates, names)
+                zip_list_sorted = sorted(zip_list, key=lambda dat: dat[0]) #sort by dates
+                print(zip_list_sorted)
+                #quit()
+
+                #axs[axs_num].plot(dates, np.zeros_like(dates),
+                #    color="k")  # Baseline and markers on it.
+
+                axs[axs_num].fill_between([0, 10000], val_min_other, val_max_other, facecolor='y', alpha=0.1)
+                axs[axs_num].fill_between([0, 10000], val_25_other, val_75_other, facecolor='y', alpha=0.25)
+                axs[axs_num].fill_between([0, 10000], val_min, val_max, facecolor='g', alpha=0.1)
+                axs[axs_num].fill_between([0, 10000], val_25, val_75, facecolor='g',alpha=0.25)
+
+                axs[axs_num].hlines(val_25_other, pmin, pmax, linestyles='dashed', colors='y')
+                axs[axs_num].hlines(val_75_other, pmin, pmax, linestyles='dashed', colors='y')
+                axs[axs_num].hlines(val_mean_other, pmin, pmax, colors='y')
+                axs[axs_num].hlines(val_min_other, pmin, pmax, linestyles='dotted', colors='y')
+                axs[axs_num].hlines(val_max_other, pmin, pmax, linestyles='dotted', colors='y')
+
+                axs[axs_num].hlines(val_25, pmin, pmax, linestyles='dashed', colors='g')
+                axs[axs_num].hlines(val_75, pmin, pmax, linestyles='dashed', colors='g')
+                axs[axs_num].hlines(val_mean, pmin, pmax, colors='g')
+                axs[axs_num].hlines(val_min, pmin, pmax, linestyles='dotted', colors='g')
+                axs[axs_num].hlines(val_max, pmin, pmax, linestyles='dotted', colors='g')
+
+            
+
+                for model in tmp['model'].unique():
+                    tmp_tmp = tmp[tmp['model']==model]
+                    #tmp_tmp.index = tmp_tmp.index.str.split('-').str[1].astype(int)
+                    tmp_tmp = tmp_tmp.sort_index()
+                    print(tmp_tmp[col].values)
+                    #axs[axs_num].plot(tmp_tmp.index/-100000, tmp_tmp[col].values,  marker='x', linewidth=1, label=model, markersize=3)
+                    axs[axs_num].plot(tmp_tmp.index, tmp_tmp[col].values,  marker='x', linewidth=1, label=model, markersize=5)
+                    #axs[axs_num].fill_between(tmp_tmp.index, tmp_tmp[col].values - tmp_tmp[col+'_std'].values, tmp_tmp[col].values + tmp_tmp[col+'_std'].values, alpha=0.2)
+                    #axs[axs_num].yticks([0, 0.25,0.5,0.75,1.0])
+                    axs[axs_num].set_ylim([0, 1])
+                    axs[axs_num].grid(True)
+                    axs[axs_num].set_xlabel('training step')
+                    axs[axs_num].set_ylabel('score value')
+
+                
+                axs[axs_num].set_title(col, fontsize='medium')#, pad=20)
+                save_title = ds_group+'__'+col
+
+            #plt.yticks([0, 0.25,0.5,0.75,1.0])
+            plt.suptitle(ds_group, fontsize='large')
+            plt.tight_layout()
+            plt.subplots_adjust(left=0.2, right=0.95, top=0.88, bottom=0.15)
+            plt.legend(ncol=2, loc=(0, -0.4))
+            #plt.legend()
+            #plt.show()
+            save_title = ds_group+'__all'
+            plt.savefig(store_path+'mean_history_threshold_'+save_title+'-'+str(10000)+'.pdf')
+    #add errors? normalize values to 0-1 shall we add weights?
+    benchmark_line_plot_history(df_real, df_synthetic, store_path)
+
+
+if __name__ == '__main__':
+    '''
     ### aggregate raw results into single files ###
     prefix = '../../Datasets/Queensland_NetFlow/results_raw/'
     experiment_id = "exp_04-real"
@@ -212,7 +471,7 @@ if __name__ == '__main__':
     load_path_gpt = prefix+experiment_id+'/'
     save_path_gpt=  prefix+experiment_id+'_results_raw.csv'
     #aggregate_raw_results(load_path_gpt, save_path_gpt, model='gpt2')   
-    
+    '''
     '''
     prefix = '../../Datasets/Queensland_NetFlow/results_raw/'
     #experiment_id = "exp_01-wgan"
@@ -221,19 +480,26 @@ if __name__ == '__main__':
     save_path_wgan =  prefix+experiment_id+'_results_raw.csv'
     #aggregate_raw_results(load_path_wgan, save_path_wgan, model='wgan')
     '''
-
-    prefix = '../../Datasets/Queensland_NetFlow/results_raw/'
+    '''
+    #prefix = '../../Datasets/Queensland_NetFlow/results_raw/'
+    prefix='test_data/'
     #experiment_id = "exp_01-wgan"
     experiment_id = "exp_05-wganbin_history"
     load_path_wgan = prefix+experiment_id+'/'
     save_path_wgan =  prefix+experiment_id+'_results_raw.csv'
     aggregate_raw_results(load_path_wgan, save_path_wgan, model='wgan-binary')
+    '''
+    ### load data
+    prefix='test_data/'
+    save_path_real = prefix+'04-real_aggregate.csv'
+    save_path_gpt = prefix+'04-gpt2_aggregate.csv'
+    save_path_wgan = prefix+'04-wganbin_aggregate.csv'
 
     ### plot data ###
 
     #load data
     df_results_real = pd.read_csv(save_path_real, header=0)
-    prefix_plot = prefix + 'plots_exp_05/'
+    prefix_plot = prefix + 'plots/'
     makedirs(prefix_plot, exist_ok=True)
     prefix_plot_detail = prefix + 'plots_detail/'
     makedirs(prefix_plot_detail, exist_ok=True)
@@ -315,28 +581,38 @@ if __name__ == '__main__':
 
     #TODO combine error counts to aggregation of results
     #get error counts
-    path_gpt = './test_data/gpt2_error_counts.csv'
+    path_gpt = 'test_data/04-gpt2_aggregate_syntax.csv'
     df_gpt_error = pd.read_csv(path_gpt, header=0)
-    df_gpt_error = split_ds_file(df_gpt_error)
+    #df_gpt_error = split_ds_file(df_gpt_error)
     print(df_gpt_error)
     
-    path_wgan = './test_data/wgan_error_counts.csv'
+    path_wgan = 'test_data/04-wganbin_aggregate_syntax.csv'
     df_wgan_error = pd.read_csv(path_wgan, header=0)
-    df_wgan_error = split_ds_file(df_wgan_error)
+    #df_wgan_error = split_ds_file(df_wgan_error)
     print(df_wgan_error)
 
-    df_gpt_error['model'] = 'gpt2'
-    df_wgan_error['model'] = 'wgan-binary'
+    #df_gpt_error['model'] = 'gpt2'
+    #df_wgan_error['model'] = 'wgan-binary'
 
-    df_combi_error = pd.concat([df_gpt_error, df_wgan_error], axis=0)
+    df_combi_error = pd.concat([df_gpt_error, df_wgan_error], axis=0).reset_index()
 
+    print('real')
     print(df_results_real)
+    print('synthetic')
     print(df_results_synthtic)
+    print('error')
+    df_combi_error.drop(columns=['file'], inplace=True)
+    df_combi_error.rename(columns={'ds':'ds_2', 'step':'sample_2'}, inplace=True)
+    df_combi_error['sample_2'] = 'step-' + df_combi_error['sample_2'].astype(str)
     print(df_combi_error)
 
-    '''
-    plot_history_mean_metric(df_results_real.copy(), df_results_synthtic.copy(), store_path+'mean_', df_combi_error)
 
+    #merge raw results + syntax checks for each (real, wgan, gpt) individually
+    df_results_synthtic = df_results_synthtic.merge(df_combi_error, how='inner', left_on=['ds_2', 'model', 'sample_2'], right_on=['ds_2', 'model', 'sample_2'])
+    print(df_results_synthtic)
+    
+    #plot_history_mean_metric(df_results_real.copy(), df_results_synthtic.copy(), store_path+'mean_', df_combi_error)
+    '''
 
 
     #quit()
