3d96823d2c11b3f37e7ee4dce17909894dba411a
maxwolf-code <maxwolfpublic@gmail.com>
2024-09-24 10:22:50 +0200

    add direct dissimilarity metrics calculation

--- dissimilarity_table_cmd.py
+++ dissimilarity_table_cmd.py
@@ -72,7 +72,6 @@ if __name__ == '__main__':
     parser.add_argument("-l", "--load_path", required=True)
     parser.add_argument("-c", "--syntax_path", required=True)
     parser.add_argument("-s", '--store_path', required=True)
-    #TODO error flows
 
     args = parser.parse_args()
     load_path = args.load_path
